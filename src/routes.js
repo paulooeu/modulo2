602d9b0f4eec7ab80be38bdeac0db510bcf02e0d
paulo <paulo.s.2011@hotmail.com.br>
2019-07-12 00:00:26 -0300

    listando agenda

--- src/routes.js
+++ src/routes.js
@@ -8,13 +8,24 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppoitmentController from './app/controllers/AppoitmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppoitmentController.store);
+routes.get('/appointments', AppoitmentController.index);
+routes.get('/schedule', ScheduleController.index);
+
 export default routes;
